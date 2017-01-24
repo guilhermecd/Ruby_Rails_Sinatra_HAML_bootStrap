5efff88bf73e732e42a1032432d1f634db5dcc63
guilhermecd <guizao.seri@gmail.com>
2017-01-24 11:47:23 -0200

    page with css

---
+++ hello.rb
@@ -0,0 +1,33 @@
+#coding: utf-8
+require 'sinatra'
+set :bind, '0.0.0.0'
+
+get '/' do
+  @titulo = "Mariane Medeiros, MSc"
+  @subtitulo = "Mestrando, UEM-Brazil"
+  haml :index
+end
+
+get '/publications' do
+  @titulo = "Mariane Medeiros, MSc"
+  @subtitulo = "Mestrando, UEM-Brazil"
+  haml :publications
+end
+
+get '/research' do
+  @titulo = "Mariane Medeiros, MSc"
+  @subtitulo = "Mestrando, UEM-Brazil"
+  haml :research
+end
+
+get '/citations' do
+  @titulo = "Mariane Medeiros, MSc"
+  @subtitulo = "Mestrando, UEM-Brazil"
+  haml :citations
+end
+
+get '/contact' do
+  @titulo = "Mariane Medeiros, MSc"
+  @subtitulo = "Mestrando, UEM-Brazil"
+  haml :contact
+end
